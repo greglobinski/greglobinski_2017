1c43a51c25fd4a5801c6ae7ea987d8775e31f6c2
Greg Lobinski <greglobinski@gmail.com>
2017-11-01 22:37:09 +0100

    redux dev-tools commented

--- src/state/createStore.js
+++ src/state/createStore.js
@@ -23,6 +23,6 @@ const initialState = {
 const createStore = () => reduxCreateStore(
   reducer, 
   initialState, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 export default createStore;
