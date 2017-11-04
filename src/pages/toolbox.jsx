cef7b0c54cdcd8ab41e78024a22f04e45a13c173
Greg Lobinski <greglobinski@gmail.com>
2017-11-04 01:46:58 +0100

    toolbox list created

--- src/pages/toolbox.jsx
+++ src/pages/toolbox.jsx
@@ -4,6 +4,28 @@ import HeadlineWithFocus from '../components/HeadlineWithFocus';
 export default () => (
   <div className="c-content-box">
     <HeadlineWithFocus>These are pieces of my toolbox</HeadlineWithFocus>
-    <p>I build web interfaces using...</p>
+    <ul className="c-content-box__list">
+      <li>JavaScript</li>
+      <li>ES6 + Babel</li>
+      <li>HTML5</li>
+      <li>CSS / CSS3</li>
+      <li>SCSS</li>
+      <li>BEM</li>
+      <li>Git / Github</li>
+      <li>React</li>
+      <li>Preact</li>
+      <li>Redux</li>
+      <li>Gatsby</li>
+      <li>Angular 2+</li>
+      <li>ArangoDB / AQL</li>
+      <li>Webpack</li>
+      <li>npm</li>
+      <li>AWS</li>
+      <li>jQuery</li>
+      <li>Bootstrap</li>
+      <li>Wordpress</li>
+      <li>Drupal</li>
+      <li>PHP</li>
+    </ul>  
   </div>
 );
